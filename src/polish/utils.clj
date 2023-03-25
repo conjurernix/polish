74433fde6d71134029f7271149d4e6123c294319
Nikolas Pafitis <pafitisnick@gmail.com>
2023-03-25 02:11:37 +0200

    Code golfing.

--- src/polish/utils.clj
+++ src/polish/utils.clj
@@ -1,12 +1,12 @@
 (ns polish.utils)
 
 (defn take-until-distinct [coll]
-  (let [helper-fn (fn helper-fn [seen coll]
-                    (if (empty? coll)
-                      []
-                      (let [x (first coll)
-                            xs (rest coll)]
-                        (if (contains? seen x)
-                          []
-                          (cons x (helper-fn (conj seen x) xs))))))]
+  (letfn [(helper-fn [seen coll]
+            (if (empty? coll)
+              []
+              (let [x (first coll)
+                    xs (rest coll)]
+                (if (contains? seen x)
+                  []
+                  (cons x (helper-fn (conj seen x) xs))))))]
     (helper-fn #{} coll)))
